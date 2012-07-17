c6474fce5c29b7a35e03ca0278b594eeca57cd00
MinRK <benjaminrk@gmail.com>
2012-07-17 08:34:31 -0500

    reorder notebooks a bit

--- notebooks/soln/matmul.py
+++
@@ -1,5 +0,0 @@
-def pdot(v, A, B):
-    v['B'] = B # push B everywhere
-    v.scatter('A', A) # scatter A
-    v.execute('C=A.dot(B)') # compute the dot-product
-    return v.gather('C', block=True) # gather the resulting sub-arrays
