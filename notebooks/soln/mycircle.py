d0615c89f913e383b5f9ed66db3c2dc7b25bc551
Bussonnier Matthias <bussonniermatthias@gmail.com>
2013-08-15 16:53:04 +0200

    rerun all to catch up with 1.0 + some modifications

--- notebooks/soln/mycircle.py
+++ notebooks/soln/mycircle.py
@@ -1,15 +1,15 @@
 class MyCircle(object):
     
     def _repr_html_(self):
-        return "&#x25CB;"
+        return "&#x25CB; (<b>html</b>)"
 
     def _repr_svg_(self):
         return """<svg width=100px height=100px>
-           <circle cx="50" cy="50" r="20" stroke="black" stroke-width="1" fill="white"/>
+           <circle cx="50" cy="50" r="20" stroke="black" stroke-width="1" fill="blue"/>
         </svg>"""
     
     def _repr_latex_(self):
-        return r"$\bigcirc$"
+        return r"$\bigcirc \LaTeX$"
 
     def _repr_javascript_(self):
         return "alert('I am a circle!');"
