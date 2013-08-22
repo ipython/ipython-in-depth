14c700a2889d4fb20adca35ad1db8b7d0acee6db
MinRK <benjaminrk@gmail.com>
2013-08-22 10:05:22 +0200

    update soln with mymagics

---
+++ notebooks/soln/mymagics.py
@@ -0,0 +1,51 @@
+# -*- encoding: utf-8 -*-
+
+import io
+import os
+import time
+
+from IPython.nbformat import current
+
+class TicToc(object):
+    def __init__(self):
+        self.tic()
+
+    def tic(self, line=''):
+        self.t0 = time.time()
+
+    def toc(self, line=''):
+        print self.format_time(time.time() - self.t0)
+
+    def format_time(self, dt):
+        if dt < 1e-6:
+            return u"%.3g ns" % (dt * 1e9)
+        elif dt < 1e-3:
+            return u"%.3g µs" % (dt * 1e6)
+        elif dt < 1:
+            return u"%.3g ms" % (dt * 1e3)
+        else:
+            return "%.3g s" % dt
+
+
+def load_notebook(filename):
+    """load a notebook object from a filename"""
+    if not os.path.exists(filename) and not filename.endswith(".ipynb"):
+        filename = filename + ".ipynb"
+    with io.open(filename) as f:
+        return current.read(f, 'json')
+
+def nbrun(line):
+    """given a filename, execute the notebook in IPython"""
+    nb = load_notebook(line)
+    ip = get_ipython()
+    for cell in nb.worksheets[0].cells:
+        if cell.cell_type == 'code':
+            ip.run_cell(cell.input, silent=True)
+    
+def load_ipython_extension(ip):
+    tictoc = TicToc()
+    ip.register_magic_function(tictoc.tic)
+    ip.register_magic_function(tictoc.toc)
+    ip.register_magic_function(nbrun)
+    ip.user_ns['load_notebook'] = load_notebook
+    
