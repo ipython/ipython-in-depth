b60fb26991b27332eceec8050d51f7f7035ef44e
Jonathan Frederic <jon.freder@gmail.com>
2014-05-01 22:42:45 -0700

    Use load instead of loadpy, fix some bugs in slns

--- notebooks/Interactive Widgets/soln/selection.py
+++ notebooks/Interactive Widgets/soln/selection.py
@@ -3,5 +3,5 @@ from IPython.display import display
 w = RadioButtonsWidget(values={"Left": 0, "Center": 1, "Right": 2}, description="Alignment:")
 display(w)
 
-print(w)
+print(w.value)
 w.value = 1
