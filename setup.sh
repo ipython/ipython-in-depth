bc85b7756976e168045f2afdf77c7511d27fd428
Jason Grout <jgrout6@bloomberg.net>
2018-07-10 03:09:18 -0500

    ipyleaflet and sidecar installation in script

--- setup.sh
+++ setup.sh
@@ -5,4 +5,7 @@ conda activate scipy18jlab
 NODE_OPTIONS=--max_old_space_size=4096 jupyter labextension install @jupyter-widgets/jupyterlab-manager jupyter-threejs ipyvolume bqplot @jupyterlab/geojson-extension @jupyterlab/fasta-extension
 
 # Optional extensions
+
+conda install -c conda-forge --override-channels --yes ipyleaflet
+pip install sidecar
 NODE_OPTIONS=--max_old_space_size=4096 jupyter labextension install jupyterlab-toc jupyter-leaflet @jupyter-widgets/jupyterlab-sidecar
