10fe47c4fdbc0310f3c25d8ee4d99e3a00b6c6c7
Matthias Bussonnier <mbussonnier@ucmerced.edu>
2019-04-28 15:02:20 -0700

    add step to rebuild

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ conda activate pycon2019
 
 NODE_OPTIONS=--max_old_space_size=4096 jupyter labextension install @jupyter-widgets/jupyterlab-manager jupyter-threejs ipyvolume bqplot @jupyterlab/geojson-extension @jupyterlab/fasta-extension
 
+jupyter lab build
+
 # Optional extensions
 
 conda install -c conda-forge --override-channels --yes ipyleaflet ipympl
