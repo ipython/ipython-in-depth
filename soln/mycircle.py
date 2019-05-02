512a731701661f1f0043502db0f207ef1ae471d0
Matthias Bussonnier <mbussonnier@ucmerced.edu>
2019-05-01 21:25:14 -0400

    fiix Custom display solution

--- soln/mycircle.py
+++ soln/mycircle.py
@@ -8,13 +8,11 @@ class MyCircle(object):
     def _repr_html_(self):
         return "&#x25CB; (<b>html</b>)"
 
-    def _repr_svg_(self):
-        return """<svg width="100px" height="100px">
-           <circle cx="50" cy="50" r="20" stroke="black" stroke-width="1" fill="blue"/>
-        </svg>"""
+    # Let's ignore that for now due to a bug in JupyterLab
+    # def _repr_svg_(self):
+    #     return """<svg width="100px" height="100px">
+    #        <circle cx="50" cy="50" r="20" stroke="black" stroke-width="1" fill="blue"/>
+    #     </svg>"""
     
     def _repr_latex_(self):
         return r"$\bigcirc \LaTeX$"
-
-    def _repr_javascript_(self):
-        return "alert('I am a circle!');"
